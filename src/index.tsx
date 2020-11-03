2f3abdbcc9fd56260f00b436298cb0aba20d60d9
Erlend Tangeraas Lygre <erly@equinor.com>
2020-11-03 12:22:46 +0100

    Run app independent from .env variables

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,18 @@ import App from './App';
 import * as authService from './services/authService';
 import './style/edsIcons';
 
-if (process.env.REACT_APP_MSAL_CLIENT_ID) {
-    (async () => {
-        try {
-            await authService.handleLogin();
-            ReactDOM.render(
-                <React.StrictMode>
-                    <>
-                        <App />
-                    </>
-                </React.StrictMode>,
-                document.getElementById('root')
-            );
-        } catch (error) {
-            console.log(error);
-        }
-    })();
-}
+(async () => {
+    try {
+        await authService.handleLogin();
+        ReactDOM.render(
+            <React.StrictMode>
+                <>
+                    <App />
+                </>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    } catch (error) {
+        console.log(error);
+    }
+})();
