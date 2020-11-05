3b3cba8a36c8215f3e9f36810a337196ec6fbb61
Erlend Tangeraas Lygre <erly@equinor.com>
2020-11-05 09:24:08 +0100

    Improve authentication structure with logic separated into hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as authService from './services/authService';
 import './style/edsIcons';
+import GlobalStyles from './style/GlobalStyles';
 
-(async () => {
-    try {
-        await authService.handleLogin();
-        ReactDOM.render(
-            <React.StrictMode>
-                <>
-                    <App />
-                </>
-            </React.StrictMode>,
-            document.getElementById('root')
-        );
-    } catch (error) {
-        console.log(error);
-    }
-})();
+ReactDOM.render(
+    <React.StrictMode>
+        <>
+            <GlobalStyles />
+            <App />
+        </>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
