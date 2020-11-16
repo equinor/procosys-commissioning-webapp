94ac6481c986b48ee64bfb11942befcbae362d81
Erlend Tangeraas Lygre <erly@equinor.com>
2020-11-16 10:11:07 +0100

    Add Errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import GlobalStyles from './style/GlobalStyles';
 import CommRouter from './CommRouter';
 import useAuthHandler from './utils/useAuthHandler';
 import LoadingPage from './components/loading/LoadingPage';
+import ErrorBoundary from './components/error/ErrorBoundary';
 
 export type ParamTypes = {
     plant: string;
@@ -26,11 +27,13 @@ function App() {
         <UserContextProvider>
             <Router>
                 <Navbar />
-                <Switch>
-                    <Route exact path={'/'} component={SelectPlant} />
-                    <Route path="/:plant" component={CommRouter} />
-                    <Route render={() => <h1>404</h1>} />
-                </Switch>
+                <ErrorBoundary>
+                    <Switch>
+                        <Route exact path={'/'} component={SelectPlant} />
+                        <Route path="/:plant" component={CommRouter} />
+                        <Route render={() => <h1>404</h1>} />
+                    </Switch>
+                </ErrorBoundary>
             </Router>
         </UserContextProvider>
     );
