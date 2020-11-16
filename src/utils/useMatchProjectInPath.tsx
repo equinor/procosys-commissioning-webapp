4b9bfddfd1191d812762c5dc3d88c67e06789f2d
Erlend Tangeraas Lygre <erly@equinor.com>
2020-11-16 11:03:02 +0100

    Match project with project in path using a custom hook

---
+++ src/utils/useMatchProjectInPath.tsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ParamTypes } from '../App';
+import PlantContext from '../contexts/PlantContext';
+import { AsyncStatus } from '../contexts/UserContext';
+import { Project } from '../services/api';
+
+class MatchProjectError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'Project is unavailable';
+    }
+}
+
+const useMatchProjectInPath = () => {
+    const { currentPlant, availableProjects } = useContext(PlantContext);
+    const { project: projectInPath } = useParams<ParamTypes>();
+    const [matchProjectStatus, setMatchProjectStatus] = useState(
+        AsyncStatus.LOADING
+    );
+    const [currentProject, setCurrentProject] = useState<Project | undefined>();
+    useEffect(() => {
+        if (!currentPlant || !availableProjects) return;
+        const matchedProject = availableProjects.find(
+            (project) => project.title === projectInPath
+        );
+        if (!matchedProject) {
+            throw new MatchProjectError(
+                'This project is either non-existent or unavailable to you. Please double check your URL and make sure you have access to this project'
+            );
+        }
+        setCurrentProject(matchedProject);
+        setMatchProjectStatus(AsyncStatus.SUCCESS);
+    }, [currentPlant, availableProjects, projectInPath]);
+    return { matchProjectStatus, currentProject };
+};
+
+export default useMatchProjectInPath;
