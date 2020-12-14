4120f933b4cd7db06a9e44927b82f40a1aa63645
Erlend Tangeraas Lygre <erly@equinor.com>
2020-12-14 12:59:48 +0100

    Add bookmarks page and bookmarks service

---
+++ src/pages/Bookmarks.tsx
@@ -0,0 +1,61 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import DetailsCard from '../components/commPkg/DetailsCard';
+import PageHeader from '../components/PageHeader';
+import { getCurrentBookmarks } from '../services/useBookmarks';
+import { Button } from '@equinor/eds-core-react';
+import { useHistory, useRouteMatch } from 'react-router-dom';
+import PlantContext from '../contexts/PlantContext';
+import Navbar from '../components/navigation/Navbar';
+
+const BookmarksWrapper = styled.main`
+    display: flex;
+    flex-direction: column;
+    & h3 {
+        text-align: center;
+    }
+    & button {
+        border-radius: 0;
+    }
+`;
+
+const Bookmarks = () => {
+    const { url } = useRouteMatch();
+    const history = useHistory();
+    const { currentProject } = useContext(PlantContext);
+    const bookmarks = getCurrentBookmarks(currentProject!.id.toString());
+    const bookmarksToDisplay = bookmarks.map((bookmark) => (
+        <DetailsCard
+            key={bookmark.pkgNumber}
+            details={{
+                description: bookmark.description,
+                commStatus: bookmark.commStatus,
+                MCStatus: bookmark.MCStatus,
+                pkgNumber: bookmark.pkgNumber,
+            }}
+            onBookmarksPage={true}
+        />
+    ));
+
+    let content = (
+        <>
+            <PageHeader title={'Bookmarks'} />
+            {bookmarksToDisplay}
+        </>
+    );
+
+    if (bookmarks.length < 1) content = <h3>No bookmarks to display.</h3>;
+    return (
+        <>
+            <Navbar rightContent="search" />
+            <BookmarksWrapper>
+                {content}
+                <Button onClick={() => history.push(`${url}/search`)}>
+                    Search for a Comm pkg
+                </Button>
+            </BookmarksWrapper>
+        </>
+    );
+};
+
+export default Bookmarks;
