6848db43ac3f6fed54a0199952a7fe37175b2457
Erlend Tangeraas Lygre <erly@equinor.com>
2020-12-16 18:23:36 +0100

    Redesign navigation footer on comm pkg page

--- src/components/commPkg/NavigationFooter.tsx
+++ src/components/commPkg/NavigationFooter.tsx
@@ -3,42 +3,64 @@ import styled from 'styled-components';
 import EdsIcon from '../EdsIcon';
 import { Button } from '@equinor/eds-core-react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
+import { COLORS, SHADOW } from '../../style/GlobalStyles';
 
-const CommPkgFooterWrapper = styled.div`
+export const CommPkgFooterWrapper = styled.div`
     width: 100%;
     position: fixed;
     bottom: 0;
     left: 0;
-    background-color: white;
-    border-top: 2px solid #f5f5f5;
+    background-image: linear-gradient(
+        to bottom,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 1) 25%,
+        rgba(255, 255, 255, 1)
+    );
     display: flex;
+    height: 100px;
+    padding-bottom: 5px;
+    justify-content: space-evenly;
+    align-items: flex-end;
 `;
 
-const FooterButton = styled(Button)<{ active: boolean }>`
+export const FooterButton = styled.button<{ active: boolean }>`
     display: flex;
     flex-direction: column;
     align-items: center;
+    border: none;
     flex: 1;
-    height: 70px;
-    background-color: ${(props) =>
-        props.active ? 'rgba(222, 237, 238, 1)' : 'initial'};
+    height: 75px;
+    max-width: 100px;
+    margin: 0 15px 0 15px;
+    padding: 20px 0 0 0;
+    transform: ${(props) => (props.active ? 'scale(1.1)' : '1')};
+    opacity: ${(props) => (props.active ? '1' : '0.8')};
+    background-color: ${(props) => (props.active ? `white` : 'white')};
+    position: relative;
     & p {
         margin: 0;
+        font-weight: ${(props) => (props.active ? 'initial' : 'initial')};
+    }
+    &:focus,
+    &:hover,
+    &:active {
+        background-color: initial;
+        outline: none;
     }
 `;
 
 const ItemCount = styled.span`
     position: absolute;
-    top: -20px;
-    right: -14px;
-    background-color: red;
+    top: 5px;
+    right: 15px;
+    background-color: #f0f0f0;
     border-radius: 15px;
     min-width: 16px;
-    padding: 5px 5px 3px 5px;
+    padding: 4px 5px 2px 5px;
     & p {
         text-align: center;
         font-size: 12px;
-        color: white;
+        color: black;
     }
 `;
 
@@ -64,47 +86,44 @@ const NavigationFooter = ({
             <FooterButton
                 data-testid="scope-button"
                 active={history.location.pathname.includes('/scope')}
-                variant="ghost"
                 onClick={() => history.push(`${url}/scope`)}
             >
+                {numberOfChecklists > 0 && (
+                    <ItemCount>
+                        <p>{numberOfChecklists}</p>
+                    </ItemCount>
+                )}
                 <EdsIcon name="list" />
                 <ButtonText>
                     <p>Scope</p>
-                    {numberOfChecklists > 0 && (
-                        <ItemCount>
-                            <p>{numberOfChecklists}</p>
-                        </ItemCount>
-                    )}
                 </ButtonText>
             </FooterButton>
             <FooterButton
                 active={history.location.pathname.includes('/tasks')}
-                variant="ghost"
                 onClick={() => history.push(`${url}/tasks`)}
             >
+                {numberOfTasks > 0 && (
+                    <ItemCount>
+                        <p>{numberOfTasks}</p>
+                    </ItemCount>
+                )}
                 <EdsIcon name="paste" />
                 <ButtonText>
                     <p>Tasks</p>
-                    {numberOfTasks > 0 && (
-                        <ItemCount>
-                            <p>{numberOfTasks}</p>
-                        </ItemCount>
-                    )}
                 </ButtonText>
             </FooterButton>
             <FooterButton
                 active={history.location.pathname.includes('/punch-list')}
-                variant="ghost"
                 onClick={() => history.push(`${url}/punch-list`)}
             >
-                <EdsIcon name="view_list" />
+                {numberOfPunches > 0 && (
+                    <ItemCount>
+                        <p>{numberOfPunches}</p>
+                    </ItemCount>
+                )}
+                <EdsIcon name="warning_filled" />
                 <ButtonText>
                     <p>Punches</p>
-                    {numberOfPunches > 0 && (
-                        <ItemCount>
-                            <p>{numberOfPunches}</p>
-                        </ItemCount>
-                    )}
                 </ButtonText>
             </FooterButton>
         </CommPkgFooterWrapper>
