2f6958bdde1771e37f4bb5ec7d6ba59a1c8f3535
Erlend Tangeraas Lygre <erly@equinor.com>
2020-12-28 19:11:28 +0100

    Add keys and horizontal scrolling for metatables.

--- src/components/checklist/MetaTable.tsx
+++ src/components/checklist/MetaTable.tsx
@@ -1,40 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ColumnLabel, Row } from '../../services/apiTypes';
-import { TextField } from '@equinor/eds-core-react';
 import styled from 'styled-components';
+import MetaTableCell from './MetaTableCell';
 
 const MetaTableWrapper = styled.table`
-    margin-bottom: 25px;
-    margin-top: -15px;
+    border-spacing: 4px;
     margin-left: auto;
-    padding-right: 20px;
-    & p {
-        float: right;
-        margin-bottom: auto;
+    & > p {
+        margin: 0;
+        padding-bottom: 12px;
     }
-    & tr {
-        height: 60px;
+    & thead {
+        & p {
+            margin: 0;
+            text-align: center;
+        }
     }
-    & th {
+
+    & tbody {
+        vertical-align: bottom;
+        & th {
+            min-width: 100px;
+            padding-right: 10px;
+            & > p {
+                margin: 0;
+                padding-bottom: 22px;
+                float: right;
+            }
+        }
+        & tr {
+            & td {
+                padding: 0;
+                margin: 0;
+            }
+        }
     }
+`;
+
+const HorizontalScroll = styled.div<{ multipleColumns: boolean }>`
+    overflow: scroll;
+    padding: 0 18px 16px 24px;
+    margin-bottom: 24px;
+    border-left: 1px solid #007079;
+    border-bottom: 1px solid #007079;
+    border-color: #000 transparent transparent transparent;
 `;
 
 type MetaTableProps = {
     labels: ColumnLabel[];
     rows: Row[];
+    isSigned: boolean;
+    checkItemId: number;
 };
 
-const MetaTable = ({ labels, rows }: MetaTableProps) => {
-    const headers = labels.map((label) => {
-        if (label) return <th key={label.id}>{label.label}</th>;
-        return <></>;
-    });
+const MetaTable = ({ labels, rows, isSigned, checkItemId }: MetaTableProps) => {
+    const headers = labels.map((label) => (
+        <React.Fragment key={label.id}>
+            {label && (
+                <th>
+                    <p>{label.label}</p>
+                </th>
+            )}
+        </React.Fragment>
+    ));
     const rowsToDisplay = rows.map((row) => {
         const cells = row.cells.map((cell) => {
             return (
-                <td>
-                    <TextField meta={cell.unit} value={cell.value} />
-                </td>
+                <MetaTableCell
+                    key={cell.columnId.toString() + row.id.toString()}
+                    isSigned={isSigned}
+                    checkItemId={checkItemId}
+                    rowId={row.id}
+                    columnId={cell.columnId}
+                    unit={cell.unit}
+                    value={cell.value}
+                />
             );
         });
         return (
@@ -48,10 +88,20 @@ const MetaTable = ({ labels, rows }: MetaTableProps) => {
     });
 
     return (
-        <MetaTableWrapper>
-            {headers.length > 1 && <tr>{headers}</tr>}
-            {rowsToDisplay}
-        </MetaTableWrapper>
+        <HorizontalScroll multipleColumns={labels.length > 2}>
+            <MetaTableWrapper>
+                {headers.length > 1 && (
+                    <thead>
+                        <tr>
+                            <th />
+                            {headers}
+                        </tr>
+                    </thead>
+                )}
+
+                <tbody>{rowsToDisplay}</tbody>
+            </MetaTableWrapper>
+        </HorizontalScroll>
     );
 };
 
