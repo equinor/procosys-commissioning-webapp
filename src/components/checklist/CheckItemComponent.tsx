22efcdb78b4a3e59359a0a0d7745d59c09d6bb07
Erlend Tangeraas Lygre <erly@equinor.com>
2020-12-28 19:09:57 +0100

    Add API calls and logic for setNA and setOK checkmarks

--- src/components/checklist/CheckItemComponent.tsx
+++ src/components/checklist/CheckItemComponent.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { CheckItem } from '../../services/apiTypes';
 import { Button } from '@equinor/eds-core-react';
 import EdsIcon from '../EdsIcon';
 import MetaTable from './MetaTable';
+import * as api from '../../services/api';
+import { CommParams } from '../../App';
+import { useParams } from 'react-router-dom';
+
+// This file has -Component suffixed to its name to avoid naming conflict with the CheckItem type.
 
 const CheckItemWrapper = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    & p {
+    & > p {
         flex: auto;
     }
 `;
@@ -22,11 +27,38 @@ const RadioGroup = styled.div`
 
 type CheckItemComponentProps = {
     item: CheckItem;
+    checklistId: number;
+    isSigned: boolean;
 };
 
-const CheckItemComponent = ({ item }: CheckItemComponentProps) => {
+const CheckItemComponent = ({ item, isSigned }: CheckItemComponentProps) => {
+    const { checklistId } = useParams<CommParams>();
+    const { plant } = useParams<CommParams>();
+    const [isNA, setIsNA] = useState(item.isNotApplicable);
+    const [isOk, setIsOk] = useState(item.isOk);
+
+    const handleSetNA = async () => {
+        try {
+            await api.postSetNA(plant, parseInt(checklistId), item.id);
+            setIsOk(false);
+            setIsNA(true);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSetOk = async () => {
+        try {
+            await api.postSetOk(plant, parseInt(checklistId), item.id);
+            setIsNA(false);
+            setIsOk(true);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     const radioButtonSelected = (
-        <EdsIcon name="radio_button_selected" color="primary" />
+        <EdsIcon name="check_circle_outlined" color="primary" />
     );
     const radioButtonUnselected = (
         <EdsIcon name="radio_button_unselected" color="primary" />
@@ -36,15 +68,19 @@ const CheckItemComponent = ({ item }: CheckItemComponentProps) => {
             <CheckItemWrapper>
                 <p>{item.text}</p>
                 <RadioGroup>
-                    {item.isOk ? (
+                    {isOk ? (
                         <Button variant="ghost">{radioButtonSelected}</Button>
                     ) : (
-                        <Button variant="ghost">{radioButtonUnselected}</Button>
+                        <Button variant="ghost" onClick={handleSetOk}>
+                            {radioButtonUnselected}
+                        </Button>
                     )}
-                    {item.isNotApplicable ? (
+                    {isNA ? (
                         <Button variant="ghost">{radioButtonSelected}</Button>
                     ) : (
-                        <Button variant="ghost">{radioButtonUnselected}</Button>
+                        <Button variant="ghost" onClick={handleSetNA}>
+                            {radioButtonUnselected}
+                        </Button>
                     )}
                 </RadioGroup>
             </CheckItemWrapper>
@@ -52,6 +88,8 @@ const CheckItemComponent = ({ item }: CheckItemComponentProps) => {
                 <MetaTable
                     labels={item.metaTable.columnLabels}
                     rows={item.metaTable.rows}
+                    isSigned={isSigned}
+                    checkItemId={item.id}
                 />
             )}
         </>
