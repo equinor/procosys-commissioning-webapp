c5cf32215b56dd0346cbe87ffc944b5aaadfc003
Erlend Tangeraas Lygre <erly@equinor.com>
2021-01-05 10:56:01 +0100

    Implement working checklist signature component

--- src/components/checklist/ChecklistSignature.tsx
+++ src/components/checklist/ChecklistSignature.tsx
@@ -3,6 +3,9 @@ import { ChecklistDetails } from '../../services/apiTypes';
 import { Button, TextField } from '@equinor/eds-core-react';
 import styled from 'styled-components';
 import { SHADOW } from '../../style/GlobalStyles';
+import * as api from '../../services/api';
+import { useParams } from 'react-router-dom';
+import { CommParams } from '../../App';
 
 const ChecklistWrapper = styled.div`
     display: flex;
@@ -11,23 +14,43 @@ const ChecklistWrapper = styled.div`
     box-shadow: ${SHADOW};
     box-sizing: border-box;
     padding: 20px;
-`;
-
-const SignatureWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    & > p {
-        font-size: 14px;
+    & button {
+        width: 100%;
+        margin-top: 15px;
     }
 `;
 
 type ChecklistSignatureProps = {
     details: ChecklistDetails;
     setIsSigned: React.Dispatch<React.SetStateAction<boolean>>;
+    isSigned: boolean;
 };
 
-const ChecklistSignature = ({ details }: ChecklistSignatureProps) => {
+const ChecklistSignature = ({
+    details,
+    setIsSigned,
+    isSigned,
+}: ChecklistSignatureProps) => {
+    const { plant, checklistId } = useParams<CommParams>();
+
+    const helperText = `Updated at ${new Date(details.updatedAt).toLocaleString(
+        'en-gb'
+    )} by ${details.updatedByFirstName} ${details.updatedByLastName}`;
+
+    const handleSignClick = async () => {
+        try {
+            if (isSigned) {
+                await api.postUnsign(plant, parseInt(checklistId));
+                setIsSigned(false);
+            } else {
+                await api.postSign(plant, parseInt(checklistId));
+                setIsSigned(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <ChecklistWrapper>
             <TextField
@@ -35,15 +58,11 @@ const ChecklistSignature = ({ details }: ChecklistSignatureProps) => {
                 multiline
                 rows={5}
                 label="Comment"
+                helperText={helperText}
             />
-            <SignatureWrapper>
-                <p>
-                    Updated at{' '}
-                    {new Date(details.updatedAt).toLocaleString('en-gb')} <br />
-                    by {details.updatedByFirstName} {details.updatedByLastName}
-                </p>
-                <Button>Sign</Button>
-            </SignatureWrapper>
+            <Button onClick={handleSignClick}>
+                {isSigned ? 'Unsign' : 'Sign'}
+            </Button>
         </ChecklistWrapper>
     );
 };
