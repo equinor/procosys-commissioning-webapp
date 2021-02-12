16f21895eacb2764795494ecf71a139fc40d1f7c
Erlend Tangeraas Lygre <erly@equinor.com>
2021-02-12 17:31:39 +0100

    Remove catch statements and accept commPkgId as string instead of number

--- src/services/procosysApi.ts
+++ src/services/procosysApi.ts
@@ -31,8 +31,7 @@ export type ProcosysApiService = {
     ) => Promise<ChecklistResponse>;
     getCommPackageDetails: (
         plantId: string,
-        commPkgNumber: string,
-        projectName: string
+        commPkgId: string
     ) => Promise<CommPkg>;
     postClear: (
         plantId: string,
@@ -53,7 +52,7 @@ export type ProcosysApiService = {
     getPunchOrganizations: (plantId: string) => Promise<PunchOrganization[]>;
     getPunchList: (
         plantId: string,
-        commPkgId: number
+        commPkgId: string
     ) => Promise<PunchPreview[]>;
     getPunchTypes: (plantId: string) => Promise<PunchType[]>;
     getPunchCategories: (plantId: string) => Promise<PunchCategory[]>;
@@ -72,10 +71,10 @@ export type ProcosysApiService = {
         rowId: number,
         value: string
     ) => Promise<void>;
-    getTasks: (plantId: string, commPkgId: number) => Promise<TaskPreview[]>;
+    getTasks: (plantId: string, commPkgId: string) => Promise<TaskPreview[]>;
     getScope: (
         plantId: string,
-        commPkgId: number
+        commPkgId: string
     ) => Promise<ChecklistPreview[]>;
     searchForCommPackage: (
         query: string,
@@ -107,43 +106,31 @@ const procosysApiService = ({
     apiVersion,
 }: ProcosysApiServiceProps): ProcosysApiService => {
     const getPlants = async () => {
-        try {
-            const { data } = await axios.get(
-                `Plants?includePlantsWithoutAccess=false${apiVersion}`
-            );
-            const camelCasedResponse = objectToCamelCase(data);
-            const camelCasedResponseWithSlug = camelCasedResponse.map(
-                (plant: Plant) => ({
-                    ...plant,
-                    slug: plant.id.substr(4),
-                })
-            );
-            return camelCasedResponseWithSlug as Plant[];
-        } catch (error) {
-            return Promise.reject(error);
-        }
+        const { data } = await axios.get(
+            `Plants?includePlantsWithoutAccess=false${apiVersion}`
+        );
+        const camelCasedResponse = objectToCamelCase(data);
+        const camelCasedResponseWithSlug = camelCasedResponse.map(
+            (plant: Plant) => ({
+                ...plant,
+                slug: plant.id.substr(4),
+            })
+        );
+        return camelCasedResponseWithSlug as Plant[];
     };
 
     const getProjectsForPlant = async (plantId: string) => {
-        try {
-            const { data } = await axios.get(
-                `Projects?plantId=${plantId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as Project[];
-        } catch (error) {
-            return Promise.reject(error);
-        }
+        const { data } = await axios.get(
+            `Projects?plantId=${plantId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as Project[];
     };
 
     const getPermissionsForPlant = async (plantId: string) => {
-        try {
-            const { data } = await axios.get(
-                `Permissions?plantId=${plantId}${apiVersion}`
-            );
-            return data as string[];
-        } catch (error) {
-            return Promise.reject(error);
-        }
+        const { data } = await axios.get(
+            `Permissions?plantId=${plantId}${apiVersion}`
+        );
+        return data as string[];
     };
 
     const searchForCommPackage = async (
@@ -152,66 +139,46 @@ const procosysApiService = ({
         plantId: string,
         cancelToken?: CancelToken
     ) => {
-        try {
-            const {
-                data,
-            } = await axios.get(
-                `CommPkg/Search?plantId=${plantId}&startsWithCommPkgNo=${query}&includeClosedProjects=false&projectId=${projectId}${apiVersion}`,
-                { cancelToken }
-            );
+        const {
+            data,
+        } = await axios.get(
+            `CommPkg/Search?plantId=${plantId}&startsWithCommPkgNo=${query}&includeClosedProjects=false&projectId=${projectId}${apiVersion}`,
+            { cancelToken }
+        );
 
-            return objectToCamelCase(data) as CommPkgSearchResults;
-        } catch (error) {
-            return Promise.reject(error);
-        }
+        return objectToCamelCase(data) as CommPkgSearchResults;
     };
 
     const getCommPackageDetails = async (
         plantId: string,
-        commPkgNumber: string,
-        projectName: string
+        commPkgId: string
     ) => {
-        try {
-            const { data } = await axios.get(
-                `CommPkg/ByCommPkgNos?plantId=${plantId}&commPkgNos=${commPkgNumber}&projectName=${projectName}${apiVersion}
+        const response = await axios.get(
+            `CommPkg?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}
 `
-            );
-            return objectToCamelCase(data[0]) as CommPkg;
-        } catch (error) {
-            return Promise.reject(error);
-        }
+        );
+        return objectToCamelCase(response.data) as CommPkg;
     };
 
-    const getScope = async (plantId: string, commPkgId: number) => {
-        try {
-            const { data } = await axios.get(
-                `CommPkg/Checklists?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as ChecklistPreview[];
-        } catch (error) {
-            return Promise.reject(error);
-        }
+    const getScope = async (plantId: string, commPkgId: string) => {
+        const { data } = await axios.get(
+            `CommPkg/Checklists?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as ChecklistPreview[];
     };
 
-    const getTasks = async (plantId: string, commPkgId: number) => {
-        try {
-            const { data } = await axios.get(
-                `CommPkg/Tasks?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as TaskPreview[];
-        } catch (error) {
-            return Promise.reject(error);
-        }
+    const getTasks = async (plantId: string, commPkgId: string) => {
+        const { data } = await axios.get(
+            `CommPkg/Tasks?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as TaskPreview[];
     };
-    const getPunchList = async (plantId: string, commPkgId: number) => {
-        try {
-            const { data } = await axios.get(
-                `CommPkg/PunchList?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as PunchPreview[];
-        } catch (error) {
-            return Promise.reject(error);
-        }
+
+    const getPunchList = async (plantId: string, commPkgId: string) => {
+        const { data } = await axios.get(
+            `CommPkg/PunchList?plantId=PCS$${plantId}&commPkgId=${commPkgId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as PunchPreview[];
     };
 
     const getChecklist = async (plantId: string, checklistId: string) => {
@@ -324,80 +291,50 @@ const procosysApiService = ({
     };
 
     const postSign = async (plantId: string, checklistId: number) => {
-        try {
-            await axios.post(
-                `CheckList/Comm/Sign?plantId=PCS$${plantId}${apiVersion}`,
-                checklistId,
-                { headers: { 'Content-Type': 'application/json' } }
-            );
-            return Promise.resolve();
-        } catch (error) {
-            console.log(error.message);
-            return Promise.reject(error.message);
-        }
+        await axios.post(
+            `CheckList/Comm/Sign?plantId=PCS$${plantId}${apiVersion}`,
+            checklistId,
+            { headers: { 'Content-Type': 'application/json' } }
+        );
+        return Promise.resolve();
     };
 
     const postUnsign = async (plantId: string, checklistId: number) => {
-        try {
-            await axios.post(
-                `CheckList/Comm/Unsign?plantId=PCS$${plantId}${apiVersion}`,
-                checklistId,
-                { headers: { 'Content-Type': 'application/json' } }
-            );
-            return Promise.resolve();
-        } catch (error) {
-            console.log(error.message);
-            return Promise.reject(error.message);
-        }
+        await axios.post(
+            `CheckList/Comm/Unsign?plantId=PCS$${plantId}${apiVersion}`,
+            checklistId,
+            { headers: { 'Content-Type': 'application/json' } }
+        );
+        return Promise.resolve();
     };
 
     const getPunchCategories = async (plantId: string) => {
-        try {
-            const { data } = await axios.get(
-                `PunchListItem/Categories?plantId=PCS$${plantId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as PunchCategory[];
-        } catch (error) {
-            console.log(error);
-            return Promise.reject(error.message);
-        }
+        const { data } = await axios.get(
+            `PunchListItem/Categories?plantId=PCS$${plantId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as PunchCategory[];
     };
 
     const getPunchTypes = async (plantId: string) => {
-        try {
-            const { data } = await axios.get(
-                `PunchListItem/Types?plantId=PCS$${plantId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as PunchType[];
-        } catch (error) {
-            console.log(error);
-            return Promise.reject(error.message);
-        }
+        const { data } = await axios.get(
+            `PunchListItem/Types?plantId=PCS$${plantId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as PunchType[];
     };
 
     const getPunchOrganizations = async (plantId: string) => {
-        try {
-            const { data } = await axios.get(
-                `PunchListItem/Organizations?plantId=PCS$${plantId}${apiVersion}`
-            );
-            return objectToCamelCase(data) as PunchOrganization[];
-        } catch (error) {
-            console.log(error);
-            return Promise.reject(error.message);
-        }
+        const { data } = await axios.get(
+            `PunchListItem/Organizations?plantId=PCS$${plantId}${apiVersion}`
+        );
+        return objectToCamelCase(data) as PunchOrganization[];
     };
 
     const postNewPunch = async (plantId: string, newPunchData: NewPunch) => {
-        try {
-            await axios.post(
-                `PunchListItem?plantId=PCS$${plantId}${apiVersion}`,
-                newPunchData
-            );
-            return Promise.resolve();
-        } catch (error) {
-            console.log(error);
-            return Promise.reject(error.message);
-        }
+        await axios.post(
+            `PunchListItem?plantId=PCS$${plantId}${apiVersion}`,
+            newPunchData
+        );
+        return Promise.resolve();
     };
 
     const getPunchItem = async (plantId: string, punchItemId: number) => {
