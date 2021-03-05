7fc680efa00d406a664504fbb433dd0cc4bd6add
Erlend Tangeraas Lygre <erly@equinor.com>
2021-03-05 18:26:24 +0100

    Create EdsCard component to remove boilerplate

--- src/components/ProcosysCard.tsx
+++ src/components/ProcosysCard.tsx
@@ -1,27 +1,7 @@
-import { Card } from '@equinor/eds-core-react';
 import React from 'react';
-import styled from 'styled-components';
 import { AsyncStatus } from '../contexts/CommAppContext';
-import { SHADOW } from '../style/GlobalStyles';
 import SkeletonLoadingPage from './loading/SkeletonLoader';
-const { CardHeader, CardHeaderTitle } = Card;
-
-export const CardWrapper = styled.article`
-    & h3,
-    label {
-        margin: 0;
-    }
-    & h5 {
-        margin: 12px 0;
-    }
-    & p {
-        margin-bottom: 8px;
-        margin-top: 0;
-    }
-    margin-bottom: 16px;
-    box-shadow: ${SHADOW};
-    border-radius: 10px;
-`;
+import EdsCard from './EdsCard';
 
 type ProcosysCardProps = {
     fetchStatus: AsyncStatus;
@@ -50,18 +30,7 @@ const ProcosysCard = ({
         }
     };
 
-    return (
-        <CardWrapper>
-            <Card>
-                <CardHeader>
-                    <CardHeaderTitle>
-                        <h3>{cardTitle}</h3>
-                    </CardHeaderTitle>
-                </CardHeader>
-                <div>{content()}</div>
-            </Card>
-        </CardWrapper>
-    );
+    return <EdsCard title={cardTitle}>{content()}</EdsCard>;
 };
 
 export default ProcosysCard;
