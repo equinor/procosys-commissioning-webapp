fee008f7f586c8c9556086d672f80c0c665c0c31
Erlend Tangeraas Lygre <erly@equinor.com>
2021-03-08 08:39:38 +0100

    Add global createObjectUrl function for tests

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom/extend-expect';
 import '@equinor/eds-core-react';
 import './setupServer';
 
+window.URL.createObjectURL = (object: any) => 'www.url.com';
+
 //Fixes MSAL interfering with the globals
 const crypto = require('crypto');
 Object.defineProperty(global, 'crypto', {
