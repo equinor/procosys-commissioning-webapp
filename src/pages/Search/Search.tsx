1499a1aca9a0efa67994082139c3eda352296061
VRiveland <vriv@equinor.com>
2021-10-16 16:13:05 +0200

    Replaced Search button with component from pcs webapp components npm package

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,11 +1,15 @@
-import React, { ChangeEvent, useEffect, useRef } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { Search as SearchField } from '@equinor/eds-core-react';
 import withAccessControl from '../../services/withAccessControl';
 import styled from 'styled-components';
 import useSearchPageFacade, { SearchStatus } from './useSearchPageFacade';
 import SearchResults from './SearchResults/SearchResults';
-import PageHeader from '../../components/PageHeader';
-import { BackButton, Navbar } from '@equinor/procosys-webapp-components';
+import {
+    BackButton,
+    Navbar,
+    SearchTypeButton,
+} from '@equinor/procosys-webapp-components';
+import Bookmarks from '../Bookmarks/Bookmarks';
 
 const SearchPageWrapper = styled.main`
     padding: 0 4%;
@@ -18,7 +22,21 @@ const SearchPageWrapper = styled.main`
     }
 `;
 
+const ButtonsWrapper = styled.div`
+    display: flex;
+    height: 60px;
+    & > button:not(:last-child) {
+        margin-right: 10px;
+    }
+`;
+
+export enum SearchType {
+    Comm = 'Comm',
+    Tag = 'Tag',
+}
+
 const Search = (): JSX.Element => {
+    const [searchType, setSearchType] = useState<string | undefined>(undefined);
     const { hits, searchStatus, query, setQuery } = useSearchPageFacade();
     const searchbarRef = useRef<HTMLInputElement>(
         document.createElement('input')
@@ -28,25 +46,32 @@ const Search = (): JSX.Element => {
         searchbarRef.current?.focus();
     }, []);
 
-    const searchHeaderToRender = (): JSX.Element => {
-        if (searchStatus === SearchStatus.SUCCESS) {
-            return (
-                <h4>
-                    Displaying {hits.items.length} out of {hits.maxAvailable}{' '}
-                    packages
-                </h4>
-            );
+    const determineComponent = (): JSX.Element => {
+        if (searchType === null) {
+            return <Bookmarks />;
         }
-        if (searchStatus === SearchStatus.LOADING) {
-            return <h4>Loading</h4>;
-        }
-        return <PageHeader title="Search" subtitle="Find a comm. pkg" />;
+        return <></>;
+        //return <SearchArea searchType={searchType} />;
     };
+
     return (
         <>
             <Navbar leftContent={<BackButton />} />
             <SearchPageWrapper>
-                {searchHeaderToRender()}
+                <p>Search for</p>
+                <ButtonsWrapper>
+                    <SearchTypeButton
+                        searchType={SearchType.Comm}
+                        currentSearchType={searchType}
+                        setCurrentSearchType={setSearchType}
+                    />
+                    <SearchTypeButton
+                        searchType={SearchType.Tag}
+                        currentSearchType={searchType}
+                        setCurrentSearchType={setSearchType}
+                    />
+                </ButtonsWrapper>
+                {determineComponent()}
                 <SearchField
                     placeholder={'For example: "1002-D01"'}
                     value={query}
