b3d3cb0952c510794c7c27820648e119502af646
sondreravndal <sondre.pedersen@bouvet.no>
2024-07-05 13:32:49 +0200

    Fix attempt

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalStyle from './GlobalStyle';
+//import GlobalStyle from './GlobalStyle';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container as HTMLElement);
-const GlobalStylesProxy: any = GlobalStyle; // TODO: remove once @types/styled-components is updated to work with react 18
 
-root.render(
-    <React.StrictMode>
-        <GlobalStylesProxy />
-        <App />
-    </React.StrictMode>
-);
+root.render(<App />);
